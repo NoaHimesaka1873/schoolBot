c963d34a2895ecb90cda23c564a632621db9c93a
Restyled.io <commits@restyled.io>
2021-06-23 06:08:26 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import sys
 
 bot = commands.Bot("학교")
 with open("settings.json", "r") as settings:
-    token = json.load(settings)['discord_token']
+    token = json.load(settings)["discord_token"]
 
 
 @bot.event
 async def on_ready():
     with open("settings.json", "r") as pre:
-        await bot.change_presence(activity=discord.Game(name=json.load(pre)['presence']))
+        await bot.change_presence(
+            activity=discord.Game(name=json.load(pre)["presence"])
+        )
 
 
 @bot.command(name="봇")
@@ -19,12 +21,13 @@ async def decodeCommand(ctx, *args):
     with open("modules.json") as _modules:
         modules = json.load(_modules)
     try:
-        sys.path.insert(1, modules[args[0]]['location'])
-        loadmd = __import__(modules[args[0]]['filename'])
-        func = getattr(loadmd, modules[args[0]]['mainfunc'])
+        sys.path.insert(1, modules[args[0]]["location"])
+        loadmd = __import__(modules[args[0]]["filename"])
+        func = getattr(loadmd, modules[args[0]]["mainfunc"])
         await func(ctx, args)
     except Exception as e:
         await ctx.send("ERROR! 명령어가 없습니다.")
         print(e)
+
 
 bot.run(token)
